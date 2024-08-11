b43b6d38f5c513c9f65accd6b34b9325a17ce26d
Oshith Roshantha <oedirisuriya@gmail.com>
2024-08-11 18:52:09 +0530

    'overlay'

--- src/Components/ForcastCard.js
+++ src/Components/ForcastCard.js
@@ -3,6 +3,11 @@ import './ForcastCard.css'
 
 export default function ForcastCard() {
   return (
-    <div className='card-conatiner'></div>
+    <div className='card-conatiner'>
+        <p className='preview-time pt-3'>2 AM</p>
+        <div className="weather-preview2"></div>
+        <p className='preview-rain'>20%</p>
+        <p className='preview-temp h1'>23&deg;</p>
+    </div>
   )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import './Main.css'
 import ForcastCard from './ForcastCard';
 
 export default function Main() {
+  const [overlayStyle, setOverlayStyle]=useState({});
+  const[locationMinimalise,setLocationMinimalise]=useState({});
+  const[openOverlayBtn,setOpenOverlayBtn]=useState(true);
+  const[closeOverlayBtn,setCloseOverlayBtn]=useState(false);
+  const[locationMinimaliseHeading,setLocationMinimaliseHeading]=useState({display:'none'});
+
+  const openOverlay = () => {
+    setOverlayStyle({
+      height: '85vh',
+      backgroundColor: '#272c4f'
+    });
+    setLocationMinimalise({
+      display:'none'
+    });
+    setLocationMinimaliseHeading({
+      display:'block'
+    });
+    setOpenOverlayBtn(false);
+    setCloseOverlayBtn(true);
+  };
+
+  const closeOverlay=()=>{
+    setOverlayStyle({});
+    setLocationMinimalise({});
+    setLocationMinimaliseHeading({
+      display:'none'
+    });
+    setCloseOverlayBtn(false);
+    setOpenOverlayBtn(true);
+  }
+
   return (
     <div className='app'>
         <div className="app-container">
-            <h1 className='main-location-name text-light display-6'>University of Kelaniya</h1>
-            <p className='main-temp text-light h1'>35&deg;</p>
-            <p className='main-weather-description'>Broken Clouds</p>
+            <h1 className='main-location-name text-light display-6' style={locationMinimalise}>University of Kelaniya</h1>
+            <p className='main-temp text-light h1' style={locationMinimalise}>35&deg;</p>
+            <p className='main-weather-description' style={locationMinimalise}>Broken Clouds</p>
+            <div className="minimalise-heading" style={locationMinimaliseHeading}>
+              <p className='minimalise-location-name text-light'>University of Kelaniya</p>
+              <div className="minimalise-description">
+                <swap className='temp'>35&deg;</swap> | <swap className='weather'>Broken Clouds</swap>
+              </div>
+            </div>
+
             <div className="min-info">
               <div className="min-info-icon">
                 <img className='min-info-icons' src="./Assets/Images/wind-speed.png"/>
@@ -25,21 +63,40 @@ export default function Main() {
               <p className='min-info-value text-light'>20%</p>
             </div>
             <div className="weather-preview">
-
             </div>
-            <div className="forcast-conatiner">
+            <div className="forcast-conatiner" style={overlayStyle}>
+              {openOverlayBtn &&
+              <div className="div-open"  onClick={openOverlay}>
+                <div className="open-btn"></div>
+              </div>}
+              {closeOverlayBtn &&
+              <div className="div-open"  onClick={closeOverlay}>
+                <div className="open-btn"></div>
+              </div>}
               <div className="preview-header">
                 <p className='forcast-type text-light'>Hourly Forcast</p>
                 <hr className='divider text-light'></hr>
               </div>
-              <Swiper
-                slidesPerView={8}
+              <Swiper className='ms-3'
+                slidesPerView={4}
                 spaceBetween={18}
                 slidesPerGroup={1}
               >
               <SwiperSlide>
-                  <ForcastCard/>    
+                  <ForcastCard/>     
               </SwiperSlide>
+              <SwiperSlide>
+                  <ForcastCard/>     
+              </SwiperSlide>
+              <SwiperSlide>
+                  <ForcastCard/>     
+              </SwiperSlide>
+              <SwiperSlide>
+                  <ForcastCard/>     
+              </SwiperSlide>
+              <SwiperSlide>
+                  <ForcastCard/>     
+              </SwiperSlide>              
               </Swiper>
             </div>
 
